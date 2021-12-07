7361edda8286aadb290770d5ebf4e4d18b69cdcb
Ericbeukes <91223305+Ericbeukes@users.noreply.github.com>
2021-12-07 10:13:04 +0200

    Delete Task 0.7.py

--- Task 0.7.py
+++
@@ -1,9 +0,0 @@
-# Convert Celsius to Fahrenheit
-celsius = float(10)
-fahrenheit = 32 + (celsius * 9/5)
-print('%.2f Celsius is: %0.2f Fahrenheit' %(celsius, fahrenheit))
-
-# Convert Fahrenheit to Celsius
-fahrenheit = float(50)
-celsius = 5/9 * (fahrenheit - 32)
-print('%.2f Fahrenheit is: %0.2f Celsius' %(fahrenheit, celsius))
