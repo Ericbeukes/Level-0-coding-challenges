45cdbe3a9e33a504b70728bbb6ecb97286979aaf
Ericbeukes <91223305+Ericbeukes@users.noreply.github.com>
2021-12-08 10:06:17 +0200

    Add files via upload

---
+++ Task 0.7.py
@@ -0,0 +1,12 @@
+def fahrenheit_to_celcius(celsius):
+    fahrenheit = 32 + (celsius * 9/5)
+    print('%0.2f Celsius is: %0.2f Fahrenheit' %(celsius,fahrenheit))
+
+
+
+def celcius_to_fahrenheit(fahrenheit):
+    celsius = 5/9 * (fahrenheit - 32)
+    print('%0.2f Fahrenheit is: %0.2f Celsius' %(fahrenheit, celsius))
+
+fahrenheit_to_celcius(10)
+celcius_to_fahrenheit(59)
