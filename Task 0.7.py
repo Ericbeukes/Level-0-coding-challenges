042eb2a173228d543cf8e45add6703ad687f4412
Ericbeukes <91223305+Ericbeukes@users.noreply.github.com>
2021-12-08 10:07:10 +0200

    Update Task 0.7.py

--- Task 0.7.py
+++ Task 0.7.py
@@ -1,9 +1,6 @@
 def fahrenheit_to_celcius(celsius):
     fahrenheit = 32 + (celsius * 9/5)
     print('%0.2f Celsius is: %0.2f Fahrenheit' %(celsius,fahrenheit))
-
-
-
 def celcius_to_fahrenheit(fahrenheit):
     celsius = 5/9 * (fahrenheit - 32)
     print('%0.2f Fahrenheit is: %0.2f Celsius' %(fahrenheit, celsius))
